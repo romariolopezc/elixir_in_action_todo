1f45245e437497002c9cb4fbb8657b2cc8803c7e
Romario López <romariolopezc@gmail.com>
2021-06-12 22:01:14 +0000

    Initial commit

---
+++ lib/todo/server.ex
@@ -0,0 +1,36 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start(list_name) do
+    GenServer.start(__MODULE__, list_name)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  @impl GenServer
+  def init(list_name) do
+    {:ok, {list_name, Todo.Database.get(list_name) || Todo.List.new()}}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {list_name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(list_name, new_state)
+    {:noreply, {list_name, new_state}}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, {list_name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {list_name, todo_list}
+    }
+  end
+end
