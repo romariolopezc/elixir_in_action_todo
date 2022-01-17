9df86c35df751c6567a118848ce12fca7a43d4e6
Romario López <romariolopezc@gmail.com>
2022-01-17 15:36:07 -0600

    Use poolboy to manage database workers

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,43 +1,33 @@
 defmodule Todo.Database do
-  @pool_size 3
   @db_folder "./persist"
 
   def child_spec(_) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
-  end
-
-  def store(key, data) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.store(key, data)
-  end
-
-  def get(key) do
-    key
-    |> choose_worker()
-    |> Todo.DatabaseWorker.get(key)
-  end
-
-  def start_link() do
     IO.puts("Starting the database server")
 
     File.mkdir_p!(@db_folder)
 
-    children = Enum.map(1..@pool_size, &worker_spec/1)
-    Supervisor.start_link(children, strategy: :one_for_one)
+    :poolboy.child_spec(
+      __MODULE__,
+      [name: {:local, __MODULE__}, worker_module: Todo.DatabaseWorker, size: 3],
+      [@db_folder]
+    )
   end
 
-  defp worker_spec(worker_id) do
-    default_worker_spec = {Todo.DatabaseWorker, {@db_folder, worker_id}}
-
-    Supervisor.child_spec(default_worker_spec, id: worker_id)
+  def store(key, data) do
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.store(worker_pid, key, data)
+      end
+    )
   end
 
-  defp choose_worker(key) do
-    :erlang.phash2(key, @pool_size) + 1
+  def get(key) do
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.get(worker_pid, key)
+      end
+    )
   end
 end
